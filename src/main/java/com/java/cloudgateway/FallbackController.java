06fca9311a1d9d405d9c55476fee851a251c8a37
Mohit Kumar <mohkumar26@publicisgroupe.net>
2022-05-29 13:00:37 +0530

    Updated code in pom.xml and application.xml

--- src/main/java/com/java/cloudgateway/FallbackController.java
+++ src/main/java/com/java/cloudgateway/FallbackController.java
@@ -1,7 +1,9 @@
 package com.java.cloudgateway;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class FallbackController {
 	
 	@RequestMapping("/orderFallBack")
